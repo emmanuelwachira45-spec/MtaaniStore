3fb060e8b4eb0223702cc365c4b105bce2870757
emmanuelwachira45-spec <emmanuelwachira45@gmail.com>
2026-07-02 11:15:02 +0300

    add new files

--- storeapp/models.py
+++ storeapp/models.py
@@ -1,3 +1,25 @@
+from datetime import date
+
 from django.db import models
+
+class Employee(models.Model):
+    GENDER_CHOICES = [
+        ('Male', 'Male'),
+        ('Female', 'Female')
+    ]
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15)
+    gender = models.CharField(max_length=15, choices=GENDER_CHOICES)
+    department = models.CharField(max_length=100)
+    job_title = models.CharField(max_length=100)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    date_employed = models.DateField()
+    address = models.TextField()
+
+    def __str__(self):
+        return self.full_name
+    
+
 
 # Create your models here.
